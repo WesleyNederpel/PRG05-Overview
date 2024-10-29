1e27a8cea62078125504a369347f1032547036bb
wesle <wesleynederpel43@gmail.com>
2024-10-29 14:00:22 +0100

    ADD login table

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,9 +45,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-    public function legosets()
-    {
-        return $this->hasMany(legoset::class);
-    }
 }
